d9972a5f5576e48c4a83fae304836de0691cf89e
Naveh Mevorach <navehmevorach@gmail.com>
2020-03-12 22:20:04 +0200

    add emails service

--- app.js
+++ app.js
@@ -22,17 +22,17 @@ app.use(
 // EMAIL SENDER FUNCTION
 const sendPasswordReset = async (user, address) => {
   const transport = nodemailer.createTransport({
-    // service: 'SendGrid',
-    // auth: {
-    //   user: process.env.SENDGRID_USERNAME,
-    //   pass: process.env.SENDGRID_PASSWORD
-    // }
-    host: process.env.EMAIL_HOST,
-    port: process.env.EMAIL_PORT,
+    service: 'SendGrid',
     auth: {
-      user: process.env.EMAIL_USERNAME,
-      pass: process.env.EMAIL_PASSWORD
+      user: process.env.SENDGRID_USERNAME,
+      pass: process.env.SENDGRID_PASSWORD
     }
+    // host: process.env.EMAIL_HOST,
+    // port: process.env.EMAIL_PORT,
+    // auth: {
+    //   user: process.env.EMAIL_USERNAME,
+    //   pass: process.env.EMAIL_PASSWORD
+    // }
   })
   // Render a template
 
